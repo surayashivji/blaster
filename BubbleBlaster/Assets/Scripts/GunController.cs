deb22e15638d64a021482792e3047ecc288f6d04
Suraya Shivji <surayashivji@gmail.com>
2017-03-05 21:27:17 -0800

    collision detection in editor

--- BubbleBlaster/Assets/Scripts/GunController.cs
+++ BubbleBlaster/Assets/Scripts/GunController.cs
@@ -6,10 +6,7 @@ public class GunController : MonoBehaviour {
 
 	public GameObject spherePrefab;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	public Transform spawnObject; // object to put on gun that will be @ the tip of it
 	
 	// Update is called once per frame
 	private void Update () {
@@ -17,8 +14,8 @@ public class GunController : MonoBehaviour {
 		if (Input.GetMouseButton(0)) {
 			// create object and give it force depending on image tracker on gon position/rotation
 			// create game object @ position/rotation of gun
-			GameObject go = Instantiate (spherePrefab, transform.position, transform.rotation) as GameObject;
-			go.GetComponent<Rigidbody> ().AddForce (transform.forward * 15, ForceMode.Impulse);
+			GameObject go = Instantiate (spherePrefab, spawnObject.position, spawnObject.rotation) as GameObject;
+			go.GetComponent<Rigidbody> ().AddForce (transform.forward * 30, ForceMode.VelocityChange);
 		}
 	}
 }
