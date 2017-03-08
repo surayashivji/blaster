2db8216c817a748c86def65b7cf56037e85d023a
Suraya Shivji <surayashivji@gmail.com>
2017-03-08 00:48:30 -0800

    basic menu, game manager singleton

---
+++ BubbleBlaster/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+	public void onPlay() {
+		GameManager.Instance.ChangeScene ("InitialScene");
+	}
+}
