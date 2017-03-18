3baa93929fe4562daf9e93b7cf5a68b5c9f64c9e
Suraya Shivji <surayashivji@gmail.com>
2017-03-17 17:45:30 -0700

    changed the gun logic: added a virtual button to the marker to shoot spheres

--- BubbleBlaster/Assets/Scripts/GunController.cs
+++ BubbleBlaster/Assets/Scripts/GunController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GunController : MonoBehaviour {
 
 	public GameObject spherePrefab;
-	bool supportsMultiTouch;
+	private bool supportsMultiTouch;
 	public Transform spawnObject; // object to put on gun that will be @ the tip of it
 
 	void Start() {
@@ -14,21 +14,23 @@ public class GunController : MonoBehaviour {
 	
 	// Update is called once per frame
 	private void Update () {
-
-		int numTouches = Input.touchCount;
-		if (numTouches > 0) {
-			for (int x = 0; x < numTouches; x++) {
-				Touch touch = Input.GetTouch (x);
-				if (touch.phase == TouchPhase.Began) {
-					spawnSphere ();
-				}
-			}
-		} else if (Input.GetMouseButtonDown (0)) { // mouse clicked somewhere in unity editor
-			spawnSphere ();
-		}
+//		int numTouches = Input.touchCount;
+//		if (numTouches > 0) {
+//			for (int x = 0; x < numTouches; x++) {
+//				
+//				Touch touch = Input.GetTouch (x);
+//				if (touch.phase == TouchPhase.Began) {
+//					spawnSphere ();
+//				}
+//
+//			}
+//		} else if (Input.GetMouseButtonDown (0)) { // mouse clicked somewhere in unity editor
+//			spawnSphere ();
+//		}
 	}
 
-	void spawnSphere() {
+	public void spawnSphere() {
+		Debug.Log ("Sphere spawned");
 		// create object and give it force depending on image tracker on gon position/rotation
 		// create game object @ position/rotation of gun
 		GameObject go = Instantiate (spherePrefab, spawnObject.position, spawnObject.rotation) as GameObject;
---
+++ BubbleBlaster/Assets/Scripts/vbScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vbScript : MonoBehaviour, IVirtualButtonEventHandler  {
+
+	// create a reference to the vb game object
+	private GameObject shootVBObject; 
+//	private GameObject gunController;
+
+	// Use this for initialization
+	void Start () {
+
+		// initialize gun controller
+//		gunController = g/ameObject.GetComponentInParent<GunController>() as GameObject;
+
+		shootVBObject = GameObject.Find ("shootButton");
+
+		// register event handler with virtual button object
+		shootVBObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+	}
+
+	// IVirtualButtonEventHandler interface methods
+
+	public void OnButtonPressed(VirtualButtonAbstractBehaviour vb) {
+		Debug.Log ("BUTTON PRESSED :)");
+
+//		GunController.spawnSphere ();
+		// cat.parent.GetComponent<CatController>().ExitConga();
+
+//		gunController.spawnSphere ();
+		gameObject.GetComponentInParent<GunController>().spawnSphere();
+	}
+
+	public void OnButtonReleased(VirtualButtonAbstractBehaviour vb) {
+	}
+}
